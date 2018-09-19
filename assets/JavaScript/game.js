cf3c8f5a8fc95160a214cb8d27acb230aeb217cf
Daniel Murphy <tugs.murphy@yahoo.com>
2018-09-19 13:45:16 -0400

    fixed and linked random gen array, changed and added a new function to fix bugs, fixed reset, Game is working and finished

--- assets/JavaScript/game.js
+++ assets/JavaScript/game.js
@@ -6,6 +6,11 @@ var counter = 0;
 var chosenNumber;
 var crystalValue = [];
 
+var blueCrystal = $("<img>");
+var clearCrystal = $("<img>");
+var purpleCrystal = $("<img>");
+var greenCrystal = $("<img>");
+
 console.log("Starting the application...")
 
 // create a random number generator (between 19 and 120)
@@ -18,9 +23,8 @@ getRandomNumber = function () {
     chosenNumber =Math.floor(Math.random()*(maxValue-minValue+1)+minValue);
     // draw the chosen number onto the DOM
     drawChosenNumber();
+    // draw the users score to the DOM
     drawTotalScore();
-    
-    
 
 }
 // create a function that will draw the randomly chosen number into the #chosen-number
@@ -54,74 +58,47 @@ updateLosses = function () {
 
 // Create a random number array between 1 and 12 for the crystals
 getCrystalNumber = function () {
-    // set the  crystalvalue array
-    var crystalValue = [];
-    // create a for loop that runs through the array four times
-    for (var i = 0; i < 4; i++) {
+
+    // set the  counter of the while loop to 0
+    var counter = 0;
+    
+    // create a while loop that runs until the array is filled with 4 unique numbers
+    while (counter < 4) {
         
             // create a random number between 1 and 12
             var randomNumber = Math.floor(Math.random() * 12) + 1;
             
-            // console.log("++++++++");
-            // console.log(randomNumber);
-            // console.log("++++++++++")
-            // push that random number into the empty array
-            
             // if statement that recoginizes duplicate entries
-            //  if (!crystalValue.includes(randomNumber)) {
-                // replaces them with new random numbers
-
+             if (!crystalValue.includes(randomNumber)) {
+    
+                // push that unique random number into the empty global array
                 crystalValue.push(randomNumber);
-            // }
+
+                // increase the counter by 1 each time
+                counter++;
+                
+            }
         }
-        console.log("_____________")
-        console.log(crystalValue);  
+        return crystalValue;
 
 }
 
-// create a function that will set data value to the crystal image
-// addCrystalValue = function () {
+giveCrystalsValue = function () {
 
-//     getCrystalNumber();
+    getCrystalNumber();
 
-//     var blueCrystal;
-//         var clearCrystal; 
-//         var purpleCrystal;
-//         var greenCrystal;
+     // // link each crystal with the data-value from the array
 
-//     for (var i = 0; i <crystalValue.length; i++) {
-        
-//         // link each crystal with the data-value from the array
-//         blueCrystal.attr("data-crystalvalue", numberOptions[i]);
-//         clearCrystal.attr("data-crystalvalue", numberOptions[i]);
-//         purpleCrystal.attr("data-crystalvalue", numberOptions[i]);
-//         greenCrystal.attr("data-crystalvalue", numberOptions[i]);
+     blueCrystal.attr("data-crystalvalue", crystalValue[0]);
+     clearCrystal.attr("data-crystalvalue", crystalValue[1]);
+     purpleCrystal.attr("data-crystalvalue", crystalValue[2]);
+     greenCrystal.attr("data-crystalvalue", crystalValue[3]);
 
-//     }
 
-//     console.log(blueCrystal);
-// };
-
+}
 
 // create a function that draws the crystal image to the DOM
 drawCrystals = function () {
-
-    
-    getCrystalNumber();
-
-   
-    
-    var test = [10, 12, 5, 1];
-    
-    // create a for loop that cycles through the crystalValue array
-    // for (var i = 0; i < crystalValue.length; i++) {
-      
-        // create the image crystal for each crystal
-       
-        var blueCrystal = $("<img>");
-        var clearCrystal = $("<img>");
-        var purpleCrystal = $("<img>");
-        var greenCrystal = $("<img>");
 
         // // link the image crystals to the CSS style
        
@@ -137,25 +114,16 @@ drawCrystals = function () {
         purpleCrystal.attr("src", "assets/Images/Purple_crystal.jpg");
         greenCrystal.attr("src", "assets/Images/Green_crystal.jpg");
 
-        // // link each crystal with the data-value from the array
-
-        blueCrystal.attr("data-crystalvalue", test[0]);
-        clearCrystal.attr("data-crystalvalue", test[1]);
-        purpleCrystal.attr("data-crystalvalue", test[2]);
-        greenCrystal.attr("data-crystalvalue", test[3]);
-
         // // draw the new crystal images to the DOM
         
         $("#crystals").append(blueCrystal);
         $("#crystals").append(clearCrystal);
         $("#crystals").append(purpleCrystal);
         $("#crystals").append(greenCrystal);
-    // };
-    console.log("_____________")
-console.log(blueCrystal.attr("data-crystalvalue", test[0]));  
+
     
 };
-
+ 
 
 // create a reset function to create a new target number and clear out the score
 reset = function () {
@@ -166,58 +134,78 @@ reset = function () {
     // reset the counter
     counter = 0;
     drawTotalScore();
+
+    // reset crystal values
+    crystalValue = [];
+    giveCrystalsValue();
     
 }
 
 
 
-
+// create a document ready function that will start the game for the user
 $(document).ready(function() {
 
-
-
+// call the getRandomNUmber function
 getRandomNumber();
+
+// Call the drawCrystal function
 drawCrystals();
 
-
-
-// // addCrystalValue();
-
-
-// getRandomNumber();
+// call the crystal value function
+giveCrystalsValue();
 
 
 
+// CONTROLS===================================================================================
+// ===========================================================================================
+
+// Create an on click function that starts when the document is readey
 $(".crystal-image").on("click", function (){
 
+    // attach the click function to each of the images created to the DOM
     var scoreMultiplier = ($(this).attr("data-crystalvalue"));
+
+    // convert the value of the scoreMultiplier value
     scoreMultiplier = parseInt(scoreMultiplier);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
+  
+    // add the scoreMultiplier to the counter that increase with every image "click"
     counter += scoreMultiplier;
 
-    // alert("New score: " + counter);
-    // console.log();
-
+    // draw the counter to the DOM for each "click"
     drawTotalScore();
 
+    // Create a win condition
+        // if the counter is equal to the chosenNumber
     if (counter === chosenNumber) {
-        alert("You win!");
 
+        // alert the user they have won and...
+        alert("Winner! Winner! Chicken Dinner!");
+
+        // increase the win counter by 1
         wins++
 
+        // update and draw the win counter to the DOM
         updateWins();
 
+        // RESET for a new game
         reset();
       }
-  
+      
+    // create a lose condition
+        // if the counter exceeds the chosenNumber
       else if (counter >= chosenNumber) {
-        alert("You lose!!");
 
+        // alert the user they have lost
+        alert("So sorry, but this is going down as a loss!!");
+
+        // increase the losses counter by 1
         losses++
 
+        // update and draw the losses counter to the DOM
         updateLosses();
 
+        // RESET for a new game
         reset();
       }
     
